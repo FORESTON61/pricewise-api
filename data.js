685900ce6eff62b1aa91aa754823bd5ad2ceaa98
FORESTON61 <ishanmandal14o@gmail.com>
2026-05-05 07:10:18 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+module.exports = {
+  iphone: {
+    currentPrice: 23267,
+    lowestPrice: 18332
+  },
+  samsung: {
+    currentPrice: 18999,
+    lowestPrice: 17999
+  },
+  laptop: {
+    currentPrice: 55999,
+    lowestPrice: 49999
+  },
+  headphones: {
+    currentPrice: 2999,
+    lowestPrice: 1999
+  }
+};
