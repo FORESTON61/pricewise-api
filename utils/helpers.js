ff6515e25c05534f99fc439c19e059112b3e7ffc
FORESTON61 <ishanmandal14o@gmail.com>
2026-05-09 03:20:07 +0530

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,50 @@
+module.exports = {
 
+  // =========================
+  // PRODUCT SLUG
+  // =========================
+
+  createSlug(product) {
+
+    return product
+      .toLowerCase()
+      .replace(/[^a-z0-9 ]/g, "")
+      .trim()
+      .replace(/\s+/g, "-");
+
+  },
+
+  // =========================
+  // PRICE DIFFERENCE %
+  // =========================
+
+  calculateDifferencePercent(
+    currentPrice,
+    lowestPrice
+  ) {
+
+    return (
+      (
+        (
+          currentPrice -
+          lowestPrice
+        ) / lowestPrice
+      ) * 100
+    ).toFixed(2);
+
+  },
+
+  // =========================
+  // VALID PRICE
+  // =========================
+
+  isValidPrice(price) {
+
+    return (
+      !isNaN(price) &&
+      price >= 1000
+    );
+
+  }
+
+};
