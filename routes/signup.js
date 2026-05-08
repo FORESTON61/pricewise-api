d16ffddfb5d66f6243962a3ba024cadc4ef84cf1
FORESTON61 <ishanmandal14o@gmail.com>
2026-05-08 06:00:25 +0530

    Update signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -1,1 +1,18 @@
+const express = require("express");
 
+module.exports = (supabase) => {
+
+  const router = express.Router();
+
+  router.get("/signup", async (req, res) => {
+
+    res.json({
+      message:
+        "Signup route working"
+    });
+
+  });
+
+  return router;
+
+};
