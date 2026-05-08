7bb7047287c6f0fb268c9894fad814e3e8474ea6
FORESTON61 <ishanmandal14o@gmail.com>
2026-05-08 06:00:57 +0530

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,1 +1,18 @@
+const express = require("express");
 
+module.exports = (supabase) => {
+
+  const router = express.Router();
+
+  router.get("/login", async (req, res) => {
+
+    res.json({
+      message:
+        "Login route working"
+    });
+
+  });
+
+  return router;
+
+};
