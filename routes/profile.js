348f9837d395412a8b722a2e0dec384a793ee965
FORESTON61 <ishanmandal14o@gmail.com>
2026-05-09 03:12:29 +0530

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -1,1 +1,49 @@
+const express = require("express");
 
+module.exports = (
+  authMiddleware
+) => {
+
+  const router =
+    express.Router();
+
+  // =========================
+  // PROFILE
+  // =========================
+
+  router.get(
+    "/profile",
+
+    authMiddleware,
+
+    async (req, res) => {
+
+      try {
+
+        res.json({
+
+          success: true,
+
+          message:
+            "Protected profile route working",
+
+          user:
+            req.user
+
+        });
+
+      } catch (error) {
+
+        res.json({
+          error:
+            error.message
+        });
+
+      }
+
+    }
+  );
+
+  return router;
+
+};
